62484901bdc74c62476d8b2331d771c00a81b609
ezra buchla <emb@catfact.net>
2020-04-29 00:21:56 -0700

    wip: add easing tables as source includes

--- test/test-fast-mover.cpp
+++ test/test-fast-mover.cpp
@@ -10,38 +10,38 @@ std::ofstream fs;
 
 void beginOutput(const std::string &path) {
     fs.open(path);
-    fs << "data = [ " << std::endl;
+    //fs << "data = [ " << std::endl;
 }
 
 void process(int count) {
     for (int i=0; i<count; ++i) {
-        fs << fm.next() << ", ";
+        fs << fm.next() << std::endl;
     }
 }
 
 void finishOutput() {
-    fs << std::endl << " ]; " << std::endl;
+    //fs << std::endl << " ]; " << std::endl;
     fs.close();
 }
 
 
 int main() {
     std::ofstream ofs;
-    beginOutput("test-fast-mover-output.m");
+    beginOutput("test-fast-mover-output.data");
 
     fm.setSampleRate(48000);
     fm.setTime(1.0);
-    fm.setTarget(1.0);
+    fm.setTarget(40.0);
 
     process(20000);
 
     fm.setTime(0.5);
-    fm.setTarget(0.5);
+    fm.setTarget(100);
 
     process(10000);
 
     fm.setTime(0.3);
-    fm.setTarget(0.8);
+    fm.setTarget(-28);
 
 
     process(20000);
